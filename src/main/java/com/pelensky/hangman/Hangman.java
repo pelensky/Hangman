a0aa9146664756102e130e5543a56c2ab2f694e8
Dan Pelensky <dan@pelensky.com>
2017-04-06 09:24:36 +0100

    Extracted game loop class

--- src/main/java/com/pelensky/hangman/Hangman.java
+++ src/main/java/com/pelensky/hangman/Hangman.java
@@ -1,119 +1,16 @@
 package com.pelensky.hangman;
 
-import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
 class Hangman {
-
-    private Game game;
-
-    Hangman(Game game){
-        this.game = game;
-    }
-
-    void startGame(){
-        this.game.newGame();
-        printWelcomeMessage();
-    }
-
-    String takeGuess(ByteArrayInputStream input){
-        char letter = (char)input.read();
-        this.game.guessLetter(letter);
-        return showCharacters();
-    }
-
-    private String showCharacters(){
-        return this.game.showCharacters();
-    }
-
-    private String showWord() {
-        return this.game.showWord();
-    }
-    int showLives(){
-        return this.game.showLives();
-    }
-
-    private boolean gameInProgress(){
-        return showLives() >= 1;
-    }
-
-    boolean gameWon(){
-        return this.game.gameWon();
-    }
-
-       boolean gameLost(){
-        return this.game.gameLost();
-    }
-
-    void gameLoop(Scanner scanner){
-        startGame();
-        while (gameInProgress()){
-            playGame(scanner);
-            if (gameWon()){
-                printWonGame();
-                break;
-            }
-            if (gameLost()) {
-                printLostGame();
-                break;
-            }
-        }
-    }
-
-    private void printInstructions(){
-        printShowWord();
-        printTakeAGuess();
-    }
-
-    private void getUserInput(Scanner scanner){
-        String guess = scanner.next();
-        ByteArrayInputStream input = new ByteArrayInputStream(guess.getBytes());
-        takeGuess(input);
-    }
-
-    private void playGame(Scanner scanner) {
-        printInstructions();
-        getUserInput(scanner);
-        printNumberOfLives();
-    }
-
-    private void printWelcomeMessage(){
-        System.out.println("Welcome to Hangman!");
-    }
-
-    private void printShowWord(){
-        System.out.println("The word is: " + showCharacters() );
-    }
-
-    private void printShowFullWord(){
-        System.out.println("The word was: " + showWord() );
-    }
-
-    private void printTakeAGuess(){
-        System.out.println("Take a guess!");
-    }
-
-    private void printWonGame(){
-        printShowFullWord();
-        System.out.println("You won!");
-    }
-
-    private void printLostGame(){
-        printShowFullWord();
-        System.out.println("You lost");
-    }
-
-    private void printNumberOfLives(){
-        System.out.println("You have " + this.game.showLives() + " lives remaining.");
-    }
 
     public static void main(String[] args){
         Word word = new Word();
         Lives lives = new Lives(5);
         Game game = new Game(word, lives);
-        Hangman hangman = new Hangman(game);
+        GameLoop gameLoop = new GameLoop(game);
         Scanner scanner = new Scanner(System.in);
-        hangman.gameLoop(scanner);
+        gameLoop.playGameLoop(scanner);
     }
 
 }
---
+++ src/test/java/com/pelensky/hangman/GameLoopTest.java
@@ -0,0 +1,126 @@
+package com.pelensky.hangman;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class GameLoopTest {
+    private GameLoop gameLoop;
+    private Game game;
+    private Word word;
+    private Lives lives;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+    @Before
+    public void setUp(){
+        word = new Word("Hello");
+        lives = new Lives(5);
+        game = new Game(word, lives);
+        gameLoop = new GameLoop(game);
+        gameLoop.startGame();
+    }
+
+    @Before
+    public void setUpStream(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void wordChangedForRightGuess(){
+        ByteArrayInputStream input = new ByteArrayInputStream("h\n".getBytes());
+        Assert.assertEquals("H____", gameLoop.takeGuess(input));
+    }
+
+    @Test
+    public void wordNotChangedForWrongGuess(){
+        ByteArrayInputStream input = new ByteArrayInputStream("a\n".getBytes());
+        Assert.assertEquals("_____", gameLoop.takeGuess(input));
+    }
+
+    @Test
+    public void livesGoDownForWrongGuess(){
+        ByteArrayInputStream input = new ByteArrayInputStream("a\n".getBytes());
+        gameLoop.takeGuess(input);
+        Assert.assertEquals(4, gameLoop.showLives());
+    }
+
+    @Test
+    public void livesStayTheSameForRightGuess(){
+        ByteArrayInputStream input = new ByteArrayInputStream("h\n".getBytes());
+        gameLoop.takeGuess(input);
+        Assert.assertEquals(5, gameLoop.showLives());
+    }
+
+    @Test
+    public void checkGameWon(){
+        ByteArrayInputStream input1 = new ByteArrayInputStream("h\n".getBytes());
+        gameLoop.takeGuess(input1);
+        ByteArrayInputStream input2 = new ByteArrayInputStream("e\n".getBytes());
+        gameLoop.takeGuess(input2);
+        ByteArrayInputStream input3 = new ByteArrayInputStream("l\n".getBytes());
+        gameLoop.takeGuess(input3);
+        ByteArrayInputStream input4 = new ByteArrayInputStream("o\n".getBytes());
+        gameLoop.takeGuess(input4);
+        Assert.assertTrue(gameLoop.gameWon());
+    }
+
+    @Test
+    public void checkGameLost(){
+        ByteArrayInputStream input1 = new ByteArrayInputStream("a\n".getBytes());
+        gameLoop.takeGuess(input1);
+        ByteArrayInputStream input2 = new ByteArrayInputStream("b\n".getBytes());
+        gameLoop.takeGuess(input2);
+        ByteArrayInputStream input3 = new ByteArrayInputStream("c\n".getBytes());
+        gameLoop.takeGuess(input3);
+        ByteArrayInputStream input4 = new ByteArrayInputStream("d\n".getBytes());
+        gameLoop.takeGuess(input4);
+        ByteArrayInputStream input5 = new ByteArrayInputStream("f\n".getBytes());
+        gameLoop.takeGuess(input5);
+        Assert.assertTrue(gameLoop.gameLost());
+    }
+
+    @Test
+    public void displaysWelcomeMessage(){
+        Scanner scanner = new Scanner("h e l o");
+        gameLoop.playGameLoop(scanner);
+        Assert.assertThat(outContent.toString(), containsString("Welcome to Hangman!\n"));
+    }
+
+    @Test
+    public void showWord(){
+        Scanner scanner = new Scanner("h e l o");
+        gameLoop.playGameLoop(scanner);
+        Assert.assertThat(outContent.toString(), containsString("The word is: _____\n"));
+    }
+
+    @Test
+    public void showOptionToTakeGuess(){
+        Scanner scanner = new Scanner("h e l o");
+        gameLoop.playGameLoop(scanner);
+        Assert.assertThat(outContent.toString(), containsString("Take a guess!\n"));
+    }
+
+    @Test
+    public void testGameLoopWinning(){
+        Scanner scanner = new Scanner("h e l o");
+        gameLoop.playGameLoop(scanner);
+        Assert.assertThat(outContent.toString(), containsString("You won!\n"));
+    }
+
+    @Test
+    public void testGameLoopLosing(){
+        Scanner scanner = new Scanner("a b c d f");
+        gameLoop.playGameLoop(scanner);
+        Assert.assertThat(outContent.toString(), containsString("You lost\n"));
+    }
+
+
+}
